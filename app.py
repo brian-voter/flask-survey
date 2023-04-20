ff40511e4797099e763890fe3d5683a61afbe3ea
Brian Voter <bvoter97@gmail.com>
2023-04-20 19:13:56 -0400

    ensures user is on the right question

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+TOTAL_QUESTIONS = len(survey.questions)
 
 @app.get("/")
 def get_root():
@@ -26,26 +27,37 @@ def post_begin():
     return redirect("/questions/0")
 
 @app.get("/questions/<int:question_num>")
-def get_questions(question_num):
+def get_question(question_num):
     """returns question page for the given question number"""
 
+    num_answered = len(session.get("responses", []))
+
+    # user already completed all questions
+    if num_answered >= len(survey.questions):
+        return redirect("/completion")
+
+    # user tried to go to the wrong question page, redirect to the next question
+    if question_num != num_answered:
+        return redirect(f"/questions/{num_answered}")
+
+    # user is requesting their next question, so we render it
     question = survey.questions[question_num]
     return render_template("question.html", question=question)
 
 
 @app.post("/answer")
 def post_answer():
-    """appends user response to responses and redirect to next question 
+    """appends user response to responses and redirect to next question
     or completion page"""
-    
+
     responses = session.get("responses", [])
     responses.append(request.form.get("answer"))
     session["responses"] = responses
-    
+
     if(len(responses) >= len(survey.questions)):
         return redirect("/completion")
-    
-    return redirect(f"/questions/{len(responses)}") # this is awful
+
+    return redirect(f"/questions/{len(responses)}")
 
 @app.get("/completion")
 def thank_you():
