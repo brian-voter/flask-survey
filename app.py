d6cb94c03270947c4735b332026d93386bb308d1
Brian Voter <bvoter97@gmail.com>
2023-04-20 14:53:02 -0400

    implements through step 4

--- app.py
+++ app.py
@@ -7,3 +7,36 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+# DOC STRINGS!!!
+
+
+@app.get("/")
+def get_root():
+
+    title = survey.title
+    instructions = survey.instructions
+
+    return render_template("survey_start.html", survey_title=title,
+                           survey_instructions=instructions)
+
+@app.post("/begin")
+def post_begin():
+
+    return redirect("/questions/0")
+
+@app.get("/questions/<question_num>")
+def get_questions(question_num):
+
+    question = survey.questions[int(question_num)]
+    return render_template("question.html", question=question)
+
+
+@app.post("/answer")
+def post_answer():
+
+    responses.append(request.form.get("answer"))
+    return redirect(f"/questions/{len(responses)}") # this is awful
